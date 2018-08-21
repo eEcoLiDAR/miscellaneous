cb555db05c2b29697b992619b67c61b9242b034e
Laurens Bogaardt <lbogaardt@gmail.com>
2018-08-21 11:36:51 +0200

    Added verification notebook

--- server_scripts/normalisation_scripts/normalise_run_tiles_redo.sh
+++ server_scripts/normalisation_scripts/normalise_run_tiles_redo.sh
@@ -1,4 +1,4 @@
-xargs --arg-file=/home/ubuntu/normalisation_scripts/normalise_tiles_redo.sh \
+nohup xargs --arg-file=/home/ubuntu/normalisation_scripts/normalise_tiles_redo.sh \
       --max-procs=1  \
       --replace \
       --verbose \
